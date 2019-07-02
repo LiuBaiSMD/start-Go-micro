f440298437c1ce22f498065dd51ccbc4780c2b1a
wuxun <84924650@qq.com>
2019-07-02 15:11:41 +0800

    上传实际service、client、proto以及proto编译代码

---
+++ goPRJ/services/hello.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "proto" // import proto生成的类
+    "github.com/micro/go-micro"
+    "golang.org/x/net/context"
+)
+
+type HelloWorld struct{}
+
+func (g *HelloWorld) Hello(ctx context.Context, req *hello_world.HelloRequest, rsp *hello_world.HelloResponse) error {
+    rsp.Greeting = "Hello World: " + req.Name
+    return nil
+} // 实现hello_world service中Hello方法
+
+func main() {
+    service := micro.NewService(
+        micro.Name("hello_world"), // 定义service的名称为hello_world
+        micro.Version("latest"),
+        micro.Metadata(map[string]string{
+            "type": "helloworld",
+        }),
+    )
+
+    service.Init() // 初始化service
+
+    hello_world.RegisterHelloWorldHandler(service.Server(), new(HelloWorld)) // 注册服务
+
+    if err := service.Run(); err != nil {
+        fmt.Println(err)
+    } // 运行服务
+}
