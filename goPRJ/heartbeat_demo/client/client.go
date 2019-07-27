9df0f0e7ba7da2593da6e107dc7cb3da562a79fc
wuxun <84924650@qq.com>
2019-07-27 10:07:01 +0800

    在心跳通信的基础上增加一个双工通信，互相发送消息

--- goPRJ/heartbeat_demo/client/client.go
+++ goPRJ/heartbeat_demo/client/client.go
@@ -1,10 +1,10 @@
+
 package main
 
 import (
 	"fmt"
 	"github.com/gogo/protobuf/proto"
 	"github.com/gorilla/websocket"
-	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/util/log"
 	"heartbeat_demo/proto"
 	"net/http"
@@ -19,6 +19,7 @@ const (
 )
 
 var (
+	clientRes heartbeat.Request
 	wsHost          = "127.0.0.1:8080"
 	wsPath          = "/heartbeat"
 	msgSeqId uint64 = 0
@@ -39,7 +40,7 @@ func NewWebsocketClient(host, path string) *Client {
 func (this *Client) SendMessage() error {
 
 	// 增加一个信号监控,检测各种退出的情况,方便通知服务器断开连接
-	interrupt := make(chan os.Signal, 2)
+	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	dialer := &websocket.Dialer{}
@@ -60,22 +61,68 @@ func (this *Client) SendMessage() error {
 				log.Fatalf("read:", err)
 				return
 			}
-			log.Fatalf("recv: %s", message)
+			if err := proto.Unmarshal(message, &clientRes); err != nil {
+				log.Logf("proto unmarshal: %s", err)
+			}
+			log.Logf("recv: %s", clientRes)
 		}
 	}()
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
+	//进行发送输入功能
+	reader:= make(chan string )
+	data := ""
+	go func(){
+		for{
+			log.Logf("please input: 	")
+			fmt.Scanf("%s",&data)
+			reader <- data
+			log.Logf("your input : %v",data)
+		}
+	}()
+	go func(){
+		d := ""
+		//ticker := time.NewTicker(time.Second*5)
+		//reader := make(chan string)
+		for{
+			select {
+			//case <-ticker.C:
+			//	err1 :=conn.WriteMessage(websocket.BinaryMessage, MsgAssembler())
+			//	if err1 != nil {
+			//		log.Printf("write close:", err1)
+			//	} else {
+			//		time.Sleep(time.Second * 5)
+			//	}
+			case d =<- reader:
+				log.Logf("----->send your input")
+				err1 :=conn.WriteMessage(websocket.BinaryMessage, MsgAssemblerReader(d))
+				if err1 != nil {
+					log.Logf("write close:", err1)
+				} else {
+					log.Logf("send input over!")
+				}
+			}
 
+		}
+	}()
+	ticker := time.NewTicker(time.Second*5)
+	defer ticker.Stop()
+	d := ""
 	for {
 		select {
 		case <-done:
-			fmt.Println("------> done!")
 			return nil
 		case <-ticker.C:
 			err := conn.WriteMessage(websocket.BinaryMessage, MsgAssembler())
 			if err != nil {
 				log.Fatalf("write:", err)
 				return nil
+			}
+		case d=<-reader:
+			log.Logf("----->send your input")
+			err1 :=conn.WriteMessage(websocket.BinaryMessage, MsgAssemblerReader(d))
+			if err1 != nil {
+				log.Logf("write close:", err1)
+			} else {
+				log.Logf("send input over!")
 			}
 		case <-interrupt:
 			log.Fatalf("interrupt")
@@ -87,13 +134,28 @@ func (this *Client) SendMessage() error {
 				log.Fatalf("write close:", err)
 				return nil
 			}
-			//select {
-			//case <-done:
-			//case <-time.After(time.Second):
-			//}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
 			return nil
 		}
 	}
+}
+
+
+func main() {
+	//// 一个简易socket连接的客户端服务
+	//service := micro.NewService(
+	//	micro.Name("go.micro.web.wsClient"),
+	//)
+	//service.Init()
+	//// 这里就开始发了别影响服务启动
+	//go msgHandler()
+	//if err := service.Run(); err != nil {
+	//	log.Logf("Run: errr %v", err)
+	//}
+	msgHandler()
 }
 
 // 组装pb的接口
@@ -114,19 +176,21 @@ func MsgAssembler() []byte {
 func msgHandler() {
 	clientWrapper := NewWebsocketClient(wsHost, wsPath)
 	if err := clientWrapper.SendMessage(); err != nil {
-		fmt.Printf("SendMessage: errr%v", err)
+		log.Logf("SendMessage: errr%v", err)
 	}
 }
-func main() {
-	// 一个简历socket连接的客户端服务
-	service := micro.NewService(
-		micro.Name("go.micro.web.wsClient"),
-	)
-	service.Init()
-	// 这里就开始发了别影响服务启动
-	go msgHandler()
-	if err := service.Run(); err != nil {
-		fmt.Printf("Run: errr%v", err)
-	}
 
+func MsgAssemblerReader(data string) []byte {
+	msgSeqId += 1
+	retPb := &heartbeat.Request{
+		ClientId: CLIENTID,
+		UserId:   USERID,
+		MsgId:    msgSeqId,
+		Data:     data,
+	}
+	byteData, err := proto.Marshal(retPb)
+	if err != nil {
+		log.Fatal("pb marshaling error: ", err)
+	}
+	return byteData
 }
--- goPRJ/heartbeat_demo/server/server.go
+++ goPRJ/heartbeat_demo/server/server.go
@@ -1,23 +1,126 @@
+//package main
+//
+//import (
+//	"github.com/gogo/protobuf/proto"
+//	"github.com/gorilla/websocket"
+//	"github.com/micro/go-micro/web"
+//	"heartbeat_demo/proto"
+//	"log"
+//	"net/http"
+//)
+//
+//var upGrader = websocket.Upgrader{
+//	CheckOrigin: func(r *http.Request) bool { return true },
+//}
+//var (
+//	clientRes heartbeat.Request
+//	msgSeqId uint64 = 0
+//	USERID uint64 = 666
+//	CLIENTID uint64 = 678
+//)
+//
+//func main() {
+//	// New web serviceru
+//
+//	service := web.NewService(
+//		web.Name("go.micro.web.heartbeat"),
+//	)
+//
+//	if err := service.Init(); err != nil {
+//		log.Fatal("Init", err)
+//	}
+//	// websocket 连接接口 web.name注册根据.分割路由路径，所以注册的路径要和name对应上
+//	service.HandleFunc("/heartbeat", conn)
+//
+//	if err := service.Run(); err != nil {
+//		log.Fatal("Run: ", err)
+//	}
+//}
+//func conn(w http.ResponseWriter, r *http.Request){
+//	//
+//	conn, err := upGrader.Upgrade(w, r, nil)
+//	if err != nil {
+//		log.Printf("upgrade: %s", err)
+//		return
+//	}
+//
+//	defer conn.Close()
+//
+//	//go func(){
+//	//	for{
+//	//		err1 :=conn.WriteMessage(websocket.BinaryMessage, MsgAssembler())
+//	//		if err1 != nil {
+//	//			log.Fatalf("write close:", err1)
+//	//		} else {
+//	//			log.Fatal("send over")
+//	//			time.Sleep(time.Second * 1)
+//	//		}
+//	//	}
+//	//}()
+//	for {
+//		log.Fatal("start ")
+//		_, buffer, err := conn.ReadMessage()
+//		if err != nil {
+//			log.Println("read1:", err)
+//			break
+//		}
+//		if err := proto.Unmarshal(buffer, &clientRes); err != nil {
+//			log.Printf("proto unmarshal: %s", err)
+//		}
+//		log.Printf("recv userId=%d MsgId=%d Data=%s", clientRes.UserId, clientRes.MsgId, clientRes.Data)
+//		//err1 :=conn.WriteMessage(websocket.BinaryMessage, MsgAssembler())
+//		//if err1 != nil {
+//		//	log.Fatalf("write close:", err1)
+//		//} else {
+//		//	log.Fatal("send over")
+//		//}
+//		//log.Fatal("again ")
+//	}
+//}
+//
+//func MsgAssembler() []byte {
+//	msgSeqId += 1
+//	retPb := &heartbeat.Request{
+//		ClientId: CLIENTID,
+//		UserId:   USERID,
+//		MsgId:    msgSeqId,
+//		Data:     "handshake:",
+//	}
+//	byteData, err := proto.Marshal(retPb)
+//	if err != nil {
+//		log.Fatal("pb marshaling error: ", err)
+//	}
+//	return byteData
+//}
+
+
+
 package main
 
 import (
+	"fmt"
 	"github.com/gogo/protobuf/proto"
 	"github.com/gorilla/websocket"
 	"github.com/micro/go-micro/web"
+	"heartbeat_demo/proto"
 	"log"
 	"net/http"
-	"heartbeat_demo/proto"
 )
 
 var upGrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
+	//对请求头进行检查
+	//CheckOrigin: func(r *http.Request) bool { return true },
 }
 var (
 	clientRes heartbeat.Request
+	msgSeqId uint64 = 0
+	USERID uint64 = 666
+	CLIENTID uint64 = 678
+
 )
 
 func main() {
-	// New web serviceru
+	// New web service
 
 	service := web.NewService(
 		web.Name("go.micro.web.heartbeat"),
@@ -33,6 +136,8 @@ func main() {
 		log.Fatal("Run: ", err)
 	}
 }
+
+
 func conn(w http.ResponseWriter, r *http.Request) {
 	//
 	conn, err := upGrader.Upgrade(w, r, nil)
@@ -42,9 +147,43 @@ func conn(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer conn.Close()
+	reader := make(chan string ,1)
+	data := ""
+	go func(){
+		for{
+			log.Printf("please input: 	")
+			fmt.Scanf("%s",&data)
+			reader <- data
+			log.Printf("your input : %v",data)
+		}
+	}()
+	go func(){
+		d := ""
+		//ticker := time.NewTicker(time.Second*5)
+		//reader := make(chan string)
+		for{
+			select {
+			//case <-ticker.C:
+			//	err1 :=conn.WriteMessage(websocket.BinaryMessage, MsgAssembler())
+			//	if err1 != nil {
+			//		log.Printf("write close:", err1)
+			//	} else {
+			//		time.Sleep(time.Second * 5)
+			//	}
+			case d =<- reader:
+				log.Printf("----->send your input")
+				err1 :=conn.WriteMessage(websocket.BinaryMessage, MsgAssemblerReader(d))
+				if err1 != nil {
+					log.Printf("write close:", err1)
+				} else {
+					log.Printf("send input over!")
+				}
+			}
 
+		}
+	}()
 	for {
-		mt, buffer, err := conn.ReadMessage()
+		_, buffer, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
@@ -52,7 +191,37 @@ func conn(w http.ResponseWriter, r *http.Request) {
 		if err := proto.Unmarshal(buffer, &clientRes); err != nil {
 			log.Printf("proto unmarshal: %s", err)
 		}
-		log.Println(mt)
-		log.Printf("%v", clientRes)
+		log.Printf("recv userId=%d MsgId=%d Data=%s", clientRes.UserId, clientRes.MsgId, clientRes.Data)
 	}
+}
+
+// 组装pb的接口
+func MsgAssembler() []byte {
+	msgSeqId += 1
+	retPb := &heartbeat.Request{
+		ClientId: CLIENTID,
+		UserId:   USERID,
+		MsgId:    msgSeqId,
+		Data:     "server handshake:",
+	}
+	byteData, err := proto.Marshal(retPb)
+	if err != nil {
+		log.Fatal("pb marshaling error: ", err)
+	}
+	return byteData
+}
+
+func MsgAssemblerReader(data string) []byte {
+	msgSeqId += 1
+	retPb := &heartbeat.Request{
+		ClientId: CLIENTID,
+		UserId:   USERID,
+		MsgId:    msgSeqId,
+		Data:     data,
+	}
+	byteData, err := proto.Marshal(retPb)
+	if err != nil {
+		log.Fatal("pb marshaling error: ", err)
+	}
+	return byteData
 }
